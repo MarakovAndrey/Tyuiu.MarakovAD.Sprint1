5cba75a19b7668c5d22adc7c41a53173f75c5933
Андрей Мараков <Tyuiu.MarakovAD@outlook.com>
2025-09-29 18:53:36 +0500

    .

--- Tyuiu.MarakovAD.Sprint1.Task1.V5.Lib/DataService.cs
+++ Tyuiu.MarakovAD.Sprint1.Task1.V5.Lib/DataService.cs
@@ -1,7 +1,7 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint1;
 namespace Tyuiu.MarakovAD.Sprint1.Task1.V5.Lib
 {
-    public class DataService : ISprint1Task1V20
+    public class DataService : ISprint1Task1V5
     {
         public double Calculate(double x, double y)
         {
