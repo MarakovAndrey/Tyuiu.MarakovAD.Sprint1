13b53de5556c6051de7be6e6e68edf0adfbafad1
Андрей Мараков <Tyuiu.MarakovAD@outlook.com>
2025-09-29 19:54:20 +0500

    Task2

---
+++ Tyuiu.MarakovAD.Sprint1.Task2.V16.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.MarakovAD.Sprint1.Task2.V16.Lib;
+namespace Tyuiu.MarakovAD.Sprint1.Task2.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExprassion()
+        {
+            DataService ds = new DataService();
+            int x = 4;
+            var res = ds.CalculatePerimetrCircle(x);
+            Assert.AreEqual(25.133, res);
+        }
+    }
+}
---
+++ Tyuiu.MarakovAD.Sprint1.Task2.V16/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.MarakovAD.Sprint1.Task2.V16.Lib;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выполнил: Мараков А. Д. | ИБКСб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #16                                                              *");
+        Console.WriteLine("* Выполнил: Мараков Андрей Дмитриевич | ИБКСб-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+        int value;
+        Console.WriteLine("Введитте значение радиуса:");
+        value = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(ds.CalculatePerimetrCircle(value));
+        Console.ReadKey();
+    }
+}
